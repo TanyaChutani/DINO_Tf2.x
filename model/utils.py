08cc19da06d8f6f4737c7bcfe2681c9f950171a8
Tanya <tanyach1997@gmail.com>
2022-07-17 00:24:10 +0530

    Update utils.py

--- model/utils.py
+++ model/utils.py
@@ -2,28 +2,31 @@ import tensorflow as tf
 
 
 class MultiCropWrapper(tf.keras.layers.Layer):
-    def __init__(self, head, backbone):
+    def __init__(self, backbone, head):
         super(MultiCropWrapper, self).__init__()
         self.head = head
         self.backbone = backbone
 
     @staticmethod
     def unique_consecutive(x):
-        neq = tf.not_equal(x[1:], x[:-1])
-        cum_sum = tf.cumsum(tf.dtypes.cast(neq, tf.int32))
-        return tf.concat([[0], cum_sum], axis=0)
+        neq = tf.math.not_equal(x, x)
+        neq = tf.cast(neq, tf.int32)
+        if neq.shape[0] > 1:
+            neq = tf.math.cumsum(tf.cast(neq, tf.int32), axis=0)
+        neq = tf.concat([[0], neq], axis=0)
+        _, _, count = tf.unique_with_counts(neq)
+        return count
 
     def call(self, x):
         if not isinstance(x, list):
             x = [x]
         unq = tf.constant([inp.shape[0] for inp in x], dtype=tf.int32)
-        unique_idx = self.unique_consecutive(unq)
-        _, _, idx_crops = tf.unique_with_counts(unique_idx)
-        idx_crops = tf.cumsum(idx_crops) + 1
-        start_idx, output = tf.constant(0), tf.reshape(
-            tf.convert_to_tensor(()), (0, 768)
-        )
-        for end_idx in idx_crops:
+        count = self.unique_consecutive(unq)
+        start_idx, output = tf.constant(0), tf.zeros((0, 768), dtype=tf.float32)
+        for end_idx in count:
+            tf.autograph.experimental.set_loop_options(
+                shape_invariants=[(output, tf.TensorShape([None, None]))]
+            )
             _out = self.backbone(
                 x[tf.get_static_value(start_idx) : tf.get_static_value(end_idx)]
             )
